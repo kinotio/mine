4bba1e227293c615bc8e5de2d61ea088bcd2cb6a
Ando <aramanamihanta@gmail.com>
2025-03-02 17:29:20 +0300

    feat: setup and configure clerk

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,12 @@
-import { NextResponse } from 'next/server'
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export function middleware(request: Request) {
-  const { pathname } = new URL(request.url)
+export default clerkMiddleware()
 
-  if (pathname === '/api/hello') {
-    return NextResponse.json({ message: 'Hello, Next.js!' })
-  }
-
-  return NextResponse.next()
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)'
+  ]
 }
