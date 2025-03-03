39b9ba063260e855fcb53146281a515b8a340ee3
Ando <hi@andostronaut.com>
2025-03-03 19:46:17 +0300

    feat: add header, footer for root layout (#14)
    * feat: add header, footer for root layout
    
    * docs: update mine banner on readme

---
+++ src/data/index.ts
@@ -0,0 +1,75 @@
+export const DATA = {
+  name: 'Mine: build and share your awesomes skills and works',
+  url: 'https://mine.kinotio.io',
+  description: 'A Developer hub for building and sharing your awesome skills and works',
+  social_url: {
+    github: 'https://github.com/kinotiodotio',
+    discord: 'https://discord.gg/cskvbKQA',
+    linkedin: 'https://linkedin.com/company/kinotiodotio',
+    x: 'https://x.com/kinotiodotio',
+    bluesky: 'https://bsky.app/profile/kinotio.io'
+  },
+  repo: 'https://github.com/kinotiodotio/mine',
+  doc_repo: 'https://github.com/kinotiodotio/mine/tree/main',
+  keywords: ['Mine', 'Open Source', 'Hub', 'Developer-Hubs', 'Skills', 'Works'],
+  color: {
+    base: '#ff6500'
+  },
+  navbar: [],
+  footer: {
+    contact: [
+      {
+        icon: 'Mail',
+        href: 'mailto:contact@kinotio.io',
+        label: 'contact@kinotio.io'
+      }
+    ],
+    help: [
+      {
+        href: '#faq',
+        name: 'FAQ'
+      },
+      {
+        href: 'https://github.com/kinotiodotio/mine/issues',
+        name: 'Issues'
+      }
+    ],
+    legal: [
+      {
+        href: '/legal/terms',
+        name: 'Terms'
+      },
+      {
+        href: '/legal/privacy',
+        name: 'Privacy'
+      }
+    ],
+    socials: [
+      {
+        icon: 'Github',
+        href: 'https://github.com/kinotiodotio',
+        name: 'github'
+      },
+      {
+        icon: 'X',
+        href: 'https://x.com/kinotiodotio',
+        label: 'X'
+      },
+      {
+        icon: 'Linkedin',
+        href: 'https://linkedin.com/company/kinotiodotio',
+        label: 'LinkedIn'
+      },
+      {
+        icon: 'MessageCircle',
+        href: 'https://discord.gg/cskvbKQA',
+        label: 'Discord'
+      },
+      {
+        icon: 'MessageCircle',
+        href: 'https://bsky.app/profile/kinotio.io',
+        label: 'Bluesky'
+      }
+    ]
+  }
+}
