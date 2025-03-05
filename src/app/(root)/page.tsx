f78a0365906d509d049076f14147a1660b32d0bb
Ando <hi@andostronaut.com>
2025-03-05 19:50:21 +0300

    feat: create how it works section (#19)

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from '@/components/mods/root/hero'
 import { Features } from '@/components/mods/root/features'
+import { HowItWorksSection } from '@/components/mods/root/how-it-works'
 
 const Page = () => {
   return (
     <div>
       <Hero />
       <Features />
+      <HowItWorksSection />
     </div>
   )
 }
