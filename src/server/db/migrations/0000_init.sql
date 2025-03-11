77afb467b9b3f1e4c684b695fdd98d5218a64576
Ando <aramanamihanta@gmail.com>
2025-03-11 16:48:46 +0300

    fix: drizzle export schema

---
+++ src/server/db/migrations/0000_init.sql
@@ -0,0 +1,32 @@
+CREATE TABLE "profiles" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"user_id" varchar(256) NOT NULL,
+	"avatarUrl" varchar(256),
+	"bannerUrl" varchar(256),
+	"name" varchar(256) NOT NULL,
+	"title" varchar(256),
+	"location" varchar(256),
+	"bio" varchar(256),
+	"email" varchar(256),
+	"website" varchar(256),
+	"github" varchar(256),
+	"x" varchar(256),
+	"linkedin" varchar(256),
+	"bluesky" varchar(256),
+	"created" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "profiles_user_id_unique" UNIQUE("user_id")
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" varchar(256) PRIMARY KEY NOT NULL,
+	"email" varchar(256) NOT NULL,
+	"username" varchar(256) NOT NULL,
+	"first_name" varchar(256) NOT NULL,
+	"last_name" varchar(256) NOT NULL,
+	"imageUrl" varchar(256),
+	"created" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
