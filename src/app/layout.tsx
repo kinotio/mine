424f10dbbd1532c0e3fdb1212ab1d82a18cd6e25
Ando <aramanamihanta@gmail.com>
2025-04-08 14:22:15 +0300

    feat: add insight script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import 'react-pdf/dist/Page/TextLayer.css'
 import { Work_Sans } from 'next/font/google'
 import { ClerkProvider, ClerkLoaded } from '@clerk/nextjs'
 import { neobrutalism } from '@clerk/themes'
+import Script from 'next/script'
 
 import { TooltipProvider } from '@/components/ui/tooltip'
 
@@ -25,7 +26,13 @@ const Layout = ({
       dynamic
       appearance={{ baseTheme: neobrutalism, variables: { colorPrimary: '#ff6500' } }}
     >
-      <html lang='en'>
+      <html lang='en' suppressHydrationWarning>
+        <Script
+          async
+          src='https://insight.kinotio.io/script.js'
+          data-website-id='623cd7a0-e21f-4779-8117-e63ae49a3638'
+        ></Script>
+
         <body className={`${workSans.className}`}>
           <ClerkLoaded>
             <TooltipProvider>{children}</TooltipProvider>
