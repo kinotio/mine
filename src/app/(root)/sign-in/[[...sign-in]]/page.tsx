eb05aa647dd8019406f448b41146a8a710653447
Ando <hi@andostronaut.com>
2025-04-09 12:10:16 +0300

    feat: add and handle clerk while loading (#179)

--- src/app/(root)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(root)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,16 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn, ClerkLoaded, ClerkLoading } from '@clerk/nextjs'
+
+import { Loader } from '@/components/loader'
 
 const Page = () => {
   return (
     <div className='w-full py-28 flex justify-center items-center'>
-      <SignIn />
+      <ClerkLoading>
+        <Loader size='xl' />
+      </ClerkLoading>
+      <ClerkLoaded>
+        <SignIn />
+      </ClerkLoaded>
     </div>
   )
 }
